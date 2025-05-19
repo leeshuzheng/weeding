23b57a8a48e1b1dbff3c4fb5635736786d90fc00
Hadi Lee <hlee@blockchain.com>
2025-05-19 20:46:34 +0800

    feat: update url

--- src/app/api/rsvp/route.ts
+++ src/app/api/rsvp/route.ts
@@ -1,29 +1,27 @@
-import { NextResponse } from "next/server"
-import { SHEETS_API_URL } from "@/app/constants"
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const body = await req.json()
-
+  const body = await req.json();
   try {
-    const response = await fetch(SHEETS_API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycby2lzdQKqF8WhA2psgzWKTfwBB_cuxOocGIsPepoxxUZ6zTZj3SB2SwCJAHQ9p_8glF/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
 
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const data = await response.json()
-    return NextResponse.json(data)
+    const data = await response.json();
+    return NextResponse.json(data);
   } catch (error) {
-    console.error("Error:", error)
-    return NextResponse.json(
-      { error: "Failed to submit RSVP" },
-      { status: 500 }
-    )
+    console.error("Error:", error);
+    return NextResponse.json({ error: "Failed to submit RSVP" }, { status: 500 });
   }
 }
