6b7509fd05fd97f369197fd1d2c4492ee46edcca
Hadi Lee <hlee@blockchain.com>
2025-05-19 20:36:32 +0800

    feat: change netlify package

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
